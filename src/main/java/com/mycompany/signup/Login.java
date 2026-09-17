2629ccf1f433eafc1d23aa2242faf44b0ec86405
Student <Student@LabVM2049939.yu5h3cukpr4ujhuqei5q2n0z4g.jnbx.internal.cloudapp.net>
2026-09-18 01:59:23 +0200

    Implemented checkUserName as well as checkPasswordComplexity

--- src/main/java/com/mycompany/signup/Login.java
+++ src/main/java/com/mycompany/signup/Login.java
@@ -10,4 +10,37 @@ package com.mycompany.signup;
  */
 class Login {
     
+    private String storedUsername;
+    private String storedPassword;
+    private String storedCellPhone;
+    private String firstName;
+    private String lastName;
+    
+    public Login(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public boolean checkUserName(String username) {
+        if (username == null) return false;
+        return username.contains("_") && username.length() <= 5;
+    }
+    
+    public boolean checkPasswordComplexity(String password) {
+        if (password == null || password.length() < 8) return false;
+        
+        boolean hasUpper = false;
+        boolean hasDigit = false;
+        boolean hasSpecail = false;
+        
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) hasUpper = true;
+            else if (Character.isDigit(c)) hasDigit = true;
+            else if (!Character.isLetterOrDigit(c)) hasSpecail = true;
+        }
+        return hasUpper && hasSpecail;
+    }
+    
+    
+    
 }
