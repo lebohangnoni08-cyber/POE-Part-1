1246c8887c7818d2edcf4bf1907b474b5b47eb38
Student <Student@LabVM2049939.yu5h3cukpr4ujhuqei5q2n0z4g.jnbx.internal.cloudapp.net>
2026-09-18 00:32:18 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/signup/SignUp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.signup;
+
+/**
+ *
+ * @author Student
+ */
+public class SignUp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
